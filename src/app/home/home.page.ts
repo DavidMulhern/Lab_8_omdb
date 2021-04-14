4fc2a71b6fe6cae5c23cfbacdb63bb5dc7806985
David Mulhern <dave_mulhern1@hotmail.com>
2021-04-14 15:15:28 +0100

    lab done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MovieService} from '../Services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  movies: any = [];
 
-  constructor() {}
+  constructor(private movieService:MovieService) {
+  }
+
+  ngOnInit(){
+    this.movieService.GetMovieData().subscribe(
+      (data)=>{
+        this.movies = data.Search;
+        console.log(this.movies);
+      }
+    );
+  }
 
 }
